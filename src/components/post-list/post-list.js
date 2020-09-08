48ed1902d72b276d4b13a66a629571d70ca052c6
andrei <andrei@example.com>
2020-09-08 21:37:38 +0300

    first commit

---
+++ src/components/post-list/post-list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostList = () =>{
+    return (
+        <ul className ="app-list list-goup">
+            <PostListItem/> 
+            <PostListItem/>    
+            <PostListItem/>       
+        </ul>
+    )
+}
---
+++ src/components/post-status-filter/post-status-filter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PostStatusFilter = () => {
+ return (
+     <div className="btn-group">
+         <button>first</button>
+         <button>Second</button>
+     </div>
+ )
+}
+
+export default PostStatusFilter;
